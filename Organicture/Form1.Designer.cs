e32b288ff47d0ad6540fa8368e9307c86b127519
Vil <49325206+VilMano@users.noreply.github.com>
2021-05-16 01:11:04 +0100

    First commit

---
+++ Organicture/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Organicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderPathBtn = new System.Windows.Forms.Button();
+            this.directoryLB = new System.Windows.Forms.ListBox();
+            this.currentDirectoryLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loadPictureBtn = new System.Windows.Forms.Button();
+            this.organiseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // folderPathBtn
+            // 
+            this.folderPathBtn.Location = new System.Drawing.Point(12, 156);
+            this.folderPathBtn.Name = "folderPathBtn";
+            this.folderPathBtn.Size = new System.Drawing.Size(115, 23);
+            this.folderPathBtn.TabIndex = 0;
+            this.folderPathBtn.Text = "Choose folder";
+            this.folderPathBtn.UseVisualStyleBackColor = true;
+            this.folderPathBtn.Click += new System.EventHandler(this.folderPathBtn_Click);
+            // 
+            // directoryLB
+            // 
+            this.directoryLB.BackColor = System.Drawing.SystemColors.Control;
+            this.directoryLB.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.directoryLB.FormattingEnabled = true;
+            this.directoryLB.ItemHeight = 15;
+            this.directoryLB.Location = new System.Drawing.Point(12, 32);
+            this.directoryLB.Name = "directoryLB";
+            this.directoryLB.Size = new System.Drawing.Size(265, 90);
+            this.directoryLB.TabIndex = 1;
+            // 
+            // currentDirectoryLbl
+            // 
+            this.currentDirectoryLbl.AutoSize = true;
+            this.currentDirectoryLbl.Location = new System.Drawing.Point(12, 129);
+            this.currentDirectoryLbl.Name = "currentDirectoryLbl";
+            this.currentDirectoryLbl.Size = new System.Drawing.Size(38, 15);
+            this.currentDirectoryLbl.TabIndex = 2;
+            this.currentDirectoryLbl.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "All directories";
+            // 
+            // loadPictureBtn
+            // 
+            this.loadPictureBtn.Location = new System.Drawing.Point(133, 156);
+            this.loadPictureBtn.Name = "loadPictureBtn";
+            this.loadPictureBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadPictureBtn.TabIndex = 4;
+            this.loadPictureBtn.Text = "Load";
+            this.loadPictureBtn.UseVisualStyleBackColor = true;
+            this.loadPictureBtn.Click += new System.EventHandler(this.loadPictureBtn_Click);
+            // 
+            // organiseBtn
+            // 
+            this.organiseBtn.Location = new System.Drawing.Point(274, 156);
+            this.organiseBtn.Name = "organiseBtn";
+            this.organiseBtn.Size = new System.Drawing.Size(75, 23);
+            this.organiseBtn.TabIndex = 5;
+            this.organiseBtn.Text = "Next";
+            this.organiseBtn.UseVisualStyleBackColor = true;
+            this.organiseBtn.Click += new System.EventHandler(this.organiseBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 191);
+            this.Controls.Add(this.organiseBtn);
+            this.Controls.Add(this.loadPictureBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentDirectoryLbl);
+            this.Controls.Add(this.directoryLB);
+            this.Controls.Add(this.folderPathBtn);
+            this.Name = "Form1";
+            this.Text = "Organicture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button folderPathBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox directoryLB;
+        private System.Windows.Forms.Label currentDirectoryLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loadPictureBtn;
+        private System.Windows.Forms.Button organiseBtn;
+    }
+}
+
---
+++ Organicture/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Organicture
+{
+    public partial class Form1 : Form
+    {
+        public string search_folder;
+        public List<string> sub_directories = new List<string>();
+        public List<string> all_pictures = new List<string>();
+        public string save_folder;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadPictureBtn.Visible = false;
+            organiseBtn.Visible = false;
+            currentDirectoryLbl.Text = "";  //always start label with an empty string
+        }
+
+        public string getPathFromExplorer()
+        {
+            string folder = "";
+
+            using (var folder_dialog = new FolderBrowserDialog())
+            {
+                DialogResult destination_folder =  folder_dialog.ShowDialog();
+                if(destination_folder == DialogResult.OK && !string.IsNullOrEmpty(folder_dialog.SelectedPath))
+                {
+                    folder = folder_dialog.SelectedPath;
+                }
+            }
+
+            return folder;
+        }
+
+        public string getDateFromFile(string file_path)      //
+        {
+            return File.GetCreationTime(file_path).ToString("dd-mm-yyyy");
+        }
+
+        public string[] getAllPictures(string folder_path)
+        {
+            return Directory.GetFiles(folder_path);   //get all files from the folder 
+        }
+
+        public void getAllFoldersAndSubFolders(string folder_path)
+        {
+            foreach (string folder in getPathToAllFolders(folder_path))
+            {
+                if (!sub_directories.Contains(folder))
+                {
+                    sub_directories.Add(folder);
+                    getAllFoldersAndSubFolders(folder);
+                }
+            }
+        }
+
+        public string[] getPathToAllFolders(string folder_path)
+        {
+            return Directory.GetDirectories(folder_path);    //get the directories in the current directory
+        }
+
+        private void folderPathBtn_Click(object sender, EventArgs e)
+        {
+            search_folder = getPathFromExplorer();      //ask the user for the directory path
+            showDirectoryChosen(search_folder);     //give a notice to the user of which folder they chose
+            changeLabelCurrentDirectory();      //display the current directory being used
+            sub_directories.Add(search_folder);     //add the root folder to the list of folders and sub folders
+
+            getAllFoldersAndSubFolders(search_folder);      //get all the sub folders inside of the root folder
+
+            directoryLB.Items.Clear();
+            foreach(string folder in sub_directories)       //TO DO: make it dynamic in a function
+            {
+                string folder_name = folder.Remove(0, 
+                    folder.LastIndexOf(
+                        search_folder.Remove(0, 
+                            search_folder.LastIndexOf("\\")+1
+                        )
+                ));
+                directoryLB.Items.Add(folder_name);
+            }
+
+            loadPictureBtn.Visible = true;
+        }
+
+        
+
+        public void showDirectoryChosen(string folder_path)
+        {
+            MessageBox.Show("You have chosen the folder: "+ Path.GetFileName(folder_path) +"");
+            changeLabelCurrentDirectory();
+        }
+
+        public void changeLabelCurrentDirectory()
+        {
+            currentDirectoryLbl.Text = search_folder;
+        }
+
+        private void loadPictureBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (string folder in sub_directories)
+                {
+                    foreach (string picture in getAllPictures(folder))
+                    {
+                        all_pictures.Add(picture);
+                    }
+                }
+
+                organiseBtn.Visible = true;
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        private void organiseBtn_Click(object sender, EventArgs e)
+        {
+            save_folder = getPathFromExplorer();
+            createFolders(save_folder);
+
+        }
+
+        public void createFolders(string path_to_folder)
+        {
+            foreach(string picture in all_pictures)
+            {
+                string pic_date = getDateFromFile(picture);
+                string pic_name = Path.GetFileName(picture);
+                string folder = save_folder + "\\" + pic_date;
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                File.Move(picture, folder + "\\" + pic_name, false);
+            }
+
+            MessageBox.Show("All done :)");
+        }
+    }
+}
